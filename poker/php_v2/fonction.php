ede8e20530baa1671994554c0c92a8c239173b98
gui_ifpa <cpxr2mail@gmail.com>
2017-09-04 16:32:17 +0200

    fonction a implementer

--- poker/php_v2/fonction.php
+++ poker/php_v2/fonction.php
@@ -1,39 +1,36 @@
 <?php
 
-    /******************************************
-     *          QUINTE FLUSH                  *
+/******************************************
+     *          QUINTE FLUSH  ROYAL           *
      ******************************************/
 
-function compareQuinte($valeur, $type="") //le paramètre $type permet vérifié la couleur ou la valeur
+function quinteFlushRoyal($valeur, $couleur) //le paramètre $type permet vérifié la couleur ou la valeur
 {
     $gagner = [true,true,true,true,true];
-    $verif = [false,false,false,false,false];
+    $gagnerval = [0=>10, 1=>11, 2=>12, 3=>13, 4=>1];
+    $verifVal = false;
+    $verifCoul = [false,false,false,false,false];
 
     for($i=0; $i<4; $i++) //on verifie les 5 cartes avec la boucle
     {
-        // $verif[$i] = false; // on remplie le tableau $verif avec "false" a chaque passage
-
-        if($type == 'valeur'){ // si je met "valeur" en paramétre, je vérifie la valeur
-
-            if($valeur[$i] == ($valeur[$i+1] -1)) // on compare si la 2eme valeur - 1 est égale à la première
+        
+            if($valeur == $gagnerval) // on compare si la 2eme valeur - 1 est égale à la première
             {
-                $verif[$i] = true; // si c'est bon on met true dans le tableau
+                $verifVal = true; // si c'est bon on met true dans le tableau
             }
-        }
-        else //sinon je vérifie la couleur
-        {
-            if($valeur[$i] == $valeur[$i+1]) //verif des couleurs
+        
+            if($couleur[$i] == $couleur[$i+1]) //verif des couleurs
             {
-                $verif[$i] = true; // si c'est bon on met true dans le tableau
+                $verifCoul[$i] = true; // si c'est bon on met true dans le tableau
             }
-        }
+        
     }
 
     $verif[4] = true;// le dernier indice du tableau n'est pas verifié, alors on le set a true pour compléter le tableau
 
-    if($gagner == $verif) //on compare les 2 tableaux et si ils sont égaux on retourne true
+    if(($gagner == $verif) && $verifVal) //on compare les 2 tableaux et si ils sont égaux on retourne true
     {
-        return true;
+        return 'quinte flush';
         //return 'vrai';
     }
     else{
@@ -42,11 +39,51 @@ function compareQuinte($valeur, $type="") //le paramètre $type permet vérifié
     }
 }
 
-    /******************************************
+
+/******************************************
+     *          QUINTE FLUSH                  *
+     ******************************************/
+
+function quinteFlush($valeur, $couleur) //le paramètre $type permet vérifié la couleur ou la valeur
+{
+    $gagner = [true,true,true,true,true];
+    $verifVal = [false,false,false,false,false];
+    $verifCoul = [false,false,false,false,false];
+
+    for($i=0; $i<4; $i++) //on verifie les 5 cartes avec la boucle
+    {
+        
+            if($valeur[$i] == ($valeur[$i+1] -1)) // on compare si la 2eme valeur - 1 est égale à la première
+            {
+                $verifVal[$i] = true; // si c'est bon on met true dans le tableau
+            }
+        
+            if($couleur[$i] == $couleur[$i+1]) //verif des couleurs
+            {
+                $verifCoul[$i] = true; // si c'est bon on met true dans le tableau
+            }
+        
+    }
+
+    $verifVal[4] = true;// le dernier indice du tableau n'est pas verifié, alors on le set a true pour compléter le tableau
+    $verifCoul[4] = true;
+
+    if(($gagner == $verifCoul) && ($gagner == $verifVal))//on compare les 2 tableaux et si ils sont égaux on retourne true
+    {
+        return 'quinte flush';
+        //return 'vrai';
+    }
+    else{
+        return false;
+        //return 'faux';
+    }
+}
+
+/******************************************
      *          CARRE                         *
      ******************************************/
 
-function compareCarre($valeur)
+function carre($valeur)
 {
     $gagner = [false,true,true,true,true];
     $verif = [false,false,false,false,false];
@@ -66,7 +103,7 @@ function compareCarre($valeur)
 
     if($verif == $gagner)
     {
-        return true;
+        return 'carré';
     }
     else
     {
@@ -74,15 +111,199 @@ function compareCarre($valeur)
     }
 }
 
-    /******************************************
+/******************************************
      *       FULL HOUSE                       *
      ******************************************/
 
-function compareFull($valeur)
-{
-   $verif = [false,false,false,false,false];
-    
+function full($valeur)
+{   
+    //je vérife si les valeur sont égalent a la forme du "full" 11122 ou 22211
+    if((($valeur[0] == $valeur[1]) && ($valeur[2] == $valeur[3]) && ($valeur[3] == $valeur[4])) || ((($valeur[0] == $valeur[1]) && ($valeur[1] == $valeur[2])) && ($valeur[3] == $valeur[4])))
+
+    {
+        return 'full';     
+    }
+    else
+    {
+        return false;
+    }
 }
+
+/******************************************
+     *          COULEUR                       *
+     ******************************************/
+
+function couleur($couleur) //le paramètre $type permet vérifié la couleur ou la valeur
+{
+    $gagner = [true,true,true,true,true];    
+    $verifCoul = [false,false,false,false,false];
+
+    for($i=0; $i<4; $i++) //on verifie les 5 cartes avec la boucle
+    {
+         if($couleur[$i] == $couleur[$i+1]) //verif des couleurs
+            {
+                $verifCoul[$i] = true; // si c'est bon on met true dans le tableau
+            }
+        
+    }
+
+    $verifCoul[4] = true;
+
+    if($gagner == $verifCoul)//on compare les 2 tableaux et si ils sont égaux on retourne true
+    {
+        return 'couleur';
+        //return 'vrai';
+    }
+    else{
+        return false;
+        //return 'faux';
+    }
+}
+
+/******************************************
+     *          QUINTE                 *
+     ******************************************/
+
+function quinte($valeur) //le paramètre $type permet vérifié la couleur ou la valeur
+{
+    $gagner = [true,true,true,true,true];
+    $verifVal = [false,false,false,false,false];    
+
+    for($i=0; $i<4; $i++) //on verifie les 5 cartes avec la boucle
+    {
+        
+            if($valeur[$i] == ($valeur[$i+1] -1)) // on compare si la 2eme valeur - 1 est égale à la première
+            {
+                $verifVal[$i] = true; // si c'est bon on met true dans le tableau
+            }        
+    }
+
+    $verifVal[4] = true;// le dernier indice du tableau n'est pas verifié, alors on le set a true pour compléter le tableau
+   
+    if($gagner == $verifVal)//on compare les 2 tableaux et si ils sont égaux on retourne true
+    {
+        return 'quinte';
+        //return 'vrai';
+    }
+    else{
+        return false;
+        //return 'faux';
+    }
+}
+
+/******************************************
+     *                BRELAN                  *
+     ******************************************/
+
+function brelan($valeur)
+{
+    $gagner = [false,false,true,true,true];
+    $verif = [false,false,false,false,false];
+
+    for($i=0; $i<4; $i++)
+    {
+        if($valeur[$i] == $valeur[$i+1])//je compare si la carte a la même valeur que la carte suivante
+        {
+            $verif[$i] = true;
+        }
+    }
+    if(($valeur[4] == $valeur[0]) ||($valeur[4] == $valeur[1]) || ($valeur[4] == $valeur[2]))//je compare la derniere carte du tableau avec la premier et la 2eme
+    {
+        $verif[4] = true;
+    }
+    sort($verif);// je tri le tableau pour qu'il corresponde au tableau $gagne
+
+    if($verif == $gagner)
+    {
+        return 'brelan';
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/******************************************
+     *            DOUBLE PAIRE                *
+     ******************************************/
+
+//PEUT SERVIR POUR VERIFIER LE BRELAN ET LE CARRE
+
+
+function doublePaire($valeur)
+{
+    $gagner = [false,true,true];
+    $verif = [false,false,false];
+
+    $retour = array_count_values($valeur); // on tri les valeurs du tableau
+    
+    $retourInv = array_values($retour); //on change les indices du tableau
+    
+    
+    for($i=0; $i<count($retourInv) ; $i++) // on verifie si il y va des valeur a 2
+    {
+        if($retourInv[$i] == 2)
+        {
+            $verif[$i] = true;
+        }
+    }
+    sort($verif);// je met le tableau dans l'ordre
+    
+       if($verif == $gagner)
+    {
+        return '2 paires';
+    }
+    else
+    {
+        return false;    
+    }
+
+}
+
+
+function carteIdentique($valeur)
+{
+    $gagner = [false,true,true];
+    $double = [false,false,false];
+
+    $retour = array_count_values($valeur); // on tri les valeurs du tableau
+    
+    $retourInv = array_values($retour); //on change les indices du tableau
+    
+    
+    for($i=0; $i<count($retourInv) ; $i++) // on verifie si il y va des valeur a 2
+    {
+        if($retourInv[$i] == 4)
+        {
+            return 'carré';
+            break;
+        }
+        elseif($retourInv[$i] == 3)
+        {
+            return 'brelan';
+            break;
+        }
+        if($retourInv[$i] == 2)
+        {
+            $verif[$i] = true;
+        }
+    }
+    sort($verif);// je met le tableau dans l'ordre
+    
+       if($verif == $gagner)
+    {
+        return '2 paires';
+    }
+    else
+    {
+        return false;    
+    }
+
+}
+
+/******************************************
+     *          VERIF MAIN                    *
+     ******************************************/
 
 function verifMain($args){
     $couleur = [];
@@ -96,20 +317,6 @@ function verifMain($args){
     }
     sort($valeur); // je trie le tableau de valeur dans  l'ordre croissant pour le comparer
 
-    $valeur5 = compareQuinte($valeur, 'valeur');
-    $couleur5 = compareQuinte($couleur);
-
-    if(($valeur5 == true) and ($couleur5 == true))
-    {
-        echo 'gagner';
-    }
-    else
-    {
-        echo 'perdu';
-    }
-
-
-    /* print_r($valeur);
-    echo '<br />';
-    print_r($couleur);*/
+    //fonction de recherche
+    
 }
--- poker/php_v2/test_fonction.php
+++ poker/php_v2/test_fonction.php
@@ -11,13 +11,13 @@ $tab1 = [0=>23, 1=>42, 2=>5, 3=>13, 4=>95];
 
 $tabTrue = [0=>true, 1=>true, 2=>false, 3=>true, 4=>true];
 
-    print_r($tabTrue);
-    echo '<br />';
-  sort($tabTrue);
-    echo '<br />';
-    print_r($tabTrue);
-    echo '<br /><br />';
-   // print_r($couleur);
+print_r($tabTrue);
+echo '<br />';
+sort($tabTrue);
+echo '<br />';
+print_r($tabTrue);
+echo '<br /><br />';
+// print_r($couleur);
 
 if($t == $tab1)
 {
@@ -45,7 +45,7 @@ $mainTest =array(
     2 => array( 'nombre_val' => 8, 'couleur_val' => 'trefle'), 
     3 => array( 'nombre_val' => 9, 'couleur_val' => 'coeur'), 
     4 => array( 'nombre_val' => 12, 'couleur_val' => 'coeur') );
-    
+
 $quinteTest =array( 
     0 => array( 'nombre_val' => 8, 'couleur_val' => 'trefle'), 
     1 => array( 'nombre_val' => 9, 'couleur_val' => 'trefle'), 
@@ -61,27 +61,27 @@ echo '<br /><br />';
 
 
 
-// TEST DE LA FONCTION compareQuinte()
+// TEST DE LA FONCTION quinteFlush()
 echo '<br  />********************************************<br />';
-echo 'TEST compareQuinte()';
+echo 'TEST quinteFlush()';
 echo '<br  />********************************************<br />';
 echo '<br /><br />';
 
 $tabChiffre=[0=>5, 1=>6, 2=>7, 3=>8, 4=>9];
 $tabCouleur=[0=>'coeur', 1=>'coeur', 2=>'coeur', 3=>'coeur', 4=>'coeur'];
 
-$verifChiffreTest = compareQuinte($tabChiffre, 'valeur');
+$verifChiffreTest = quinteFlush($tabChiffre, 'valeur');
 echo $verifChiffreTest;
 echo '<br /><br />';
 
-$verifCouleurTest = compareQuinte($tabCouleur);
+$verifCouleurTest = quinteFlush($tabChiffre, $tabCouleur);
 echo $verifCouleurTest;
 echo '<br /><br />';
 
 
-// TEST compareCarre()
+// TEST Carre()
 echo '********************************************<br />';
-echo 'TEST compareCarre()';
+echo 'TEST Carre()';
 echo '<br  />********************************************<br />';
 echo '<br /><br />';
 
@@ -89,18 +89,132 @@ $carreTest = [0=>23, 1=>23, 2=>15, 3=>23, 4=>23];
 $brelanTest = [0=>23, 1=>23, 2=>15, 3=>13, 4=>23];
 
 
-$resErreurCarre = compareCarre($carreTest);
+$resErreurCarre = carre($carreTest);
 echo 'Doit retourner \'perdu\' : ' . $resErreurCarre . ' .';
 echo '<br /><br />';
 
-$resErreurCarre = compareCarre($brelanTest);
+$resErreurCarre = carre($brelanTest);
 echo 'Doit retourner \'perdu\' : ' . $resErreurCarre . ' .';
 echo '<br /><br />';
 
 sort($carreTest);
 
-$resCarre = compareCarre($carreTest);
+$resCarre = carre($carreTest);
 echo 'Doit retourner \'true\' : ' . $resCarre . ' .';
 echo '<br /><br />';
 
 
+
+//TEST DE RECHERCHE DANS UN TABLEAU
+echo '********************************************<br />';
+echo 'TEST DE RECHERCHE DANS UN TABLEAU';
+echo '<br  />********************************************<br />';
+echo '<br /><br />';
+
+$json = json_encode($carreTest);
+echo $json;
+
+$quite = array(
+    0=> array(0=>1, 1=>2, 2=>3, 3=>4, 4=>5),
+    1=> array(0=>2, 1=>3, 2=>4, 3=>5, 4=>6),
+    2=> array(0=>3, 1=>4, 2=>5, 3=>6, 4=>7),
+    3=> array(0=>4, 1=>5, 2=>6, 3=>7, 4=>8),
+    3=> array(0=>5, 1=>6, 2=>7, 3=>8, 4=>9)
+
+);
+
+if(!array_search($carreTest, $quite))
+{
+    echo 'La main est perdante';
+}
+else{
+    echo 'Gagner !!!';
+}
+echo '<br /><br />';
+
+
+//TEST DE LA FONCTION Full()
+echo '********************************************<br />';
+echo 'TEST DE Full()';
+echo '<br  />********************************************<br />';
+echo '<br /><br />';
+$full1=[0=>2, 1=>2, 2=>9, 3=>9, 4=>9];
+$full2=[0=>2, 1=>2, 2=>2, 3=>9, 4=>9];
+$fullErreur=[0=>2, 1=>2, 2=>2, 3=>2, 4=>9];
+
+$verifFull = full($full1);
+echo 'Doit retourner \'gagner\' : ' . $verifFull . ' .';
+echo '<br />';
+
+$verifFull = full($fullErreur);
+echo 'Doit retourner \'perdu\' : ' . $verifFull . ' .';
+echo '<br />';
+
+$verifFull = full($full2);
+echo 'Doit retourner \'gagner\' : ' . $verifFull . ' .';
+echo '<br /><br />';
+
+// TEST Brelan()
+echo '********************************************<br />';
+echo 'TEST Brelan()';
+echo '<br  />********************************************<br />';
+echo '<br /><br />';
+sort($brelanTest);
+sort($carreTest);
+
+$brelanTestverif = brelan($brelanTest);
+echo 'doit retrouner \'gagner\' : ' . $brelanTestverif . ' .<br /><br />';
+
+$brelanTestverif = brelan($carreTest);
+echo 'doit retrouner \'perdu\' : ' . $brelanTestverif . ' .<br /><br />';
+
+
+
+// TEST DoublePaire()
+echo '********************************************<br />';
+echo 'TEST DoublePaire()';
+echo '<br  />********************************************<br />';
+echo '<br /><br />';
+
+$doublePaire1=[0=>2, 1=>2, 2=>15, 3=>9, 4=>9];
+$doublePaire2=[0=>43, 1=>12, 2=>12, 3=>9, 4=>43];
+
+$resPaire = doublePaire($doublePaire1);
+echo 'doit retrouner \'gagner\' : ' . $resPaire . ' .<br /><br />';
+
+$resPaire = doublePaire($doublePaire2);
+echo 'doit retrouner \'gagner\' : ' . $resPaire . ' .<br /><br />';
+
+$resPaire = doublePaire($brelanTest);
+echo 'doit retrouner \'perdu\' : ' . $resPaire . ' .<br /><br />';
+
+$tar = array_count_values($doublePaire1);
+print_r($tar);
+echo '<br />';
+$tar2 = array_values($tar);
+print_r($tar2);
+echo '<br /><br />';
+
+
+
+// TEST carteIdentique()
+echo '********************************************<br />';
+echo 'TEST carteIdentique()';
+echo '<br  />********************************************<br />';
+echo '<br /><br />';
+
+$cIdent = carteIdentique($carreTest);
+echo $cIdent;
+echo '<br /><br />';
+
+$cIdent = carteIdentique($doublePaire1);
+echo $cIdent;
+echo '<br /><br />';
+
+$cIdent = carteIdentique($brelanTest);
+echo $cIdent;
+echo '<br /><br />';
+
+$cIdent = carteIdentique($doublePaire2);
+echo $cIdent;
+echo '<br /><br />';
